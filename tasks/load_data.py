1ed0de280ac9410d1d7b714eb179061f4f3bebca
Laszlo Sragner <sragner@gmail.com>
2020-11-29 12:48:18 +0000

    load data source task created

---
+++ tasks/load_data.py
@@ -0,0 +1,18 @@
+from hypergol import Source
+
+
+class LoadData(Source):
+
+    def __init__(self, exampleParameter, *args, **kwargs):
+        super(LoadData, self).__init__(*args, **kwargs)
+        # TODO: Source tasks are single threaded, no need for members to be pickle-able
+        self.exampleParameter = exampleParameter
+
+    def source_iterator(self):
+        raise NotImplementedError(f'{self.__class__.__name__} must implement source_iterator()')
+        # TODO: use yield in this function instead of return while your are consuming your source data
+        yield exampleData
+
+    def run(self, data):
+        raise NotImplementedError(f'{self.__class__.__name__} must implement run()')
+        return exampleOutputObject
