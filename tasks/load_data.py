7e714209372bae63c4f37cb928c4fa2ea797743a
xLaszlo <sragner@gmail.com>
2020-11-29 20:07:13 +0000

    Attempt for faster load with pandas (#2)
    * attempt for faster load
    
    * attempt for faster load
    
    * attempt for faster load
    
    * attempt for faster load
    
    * attempt for faster load
    
    * attempt for faster load
    
    * attempt for faster load

--- tasks/load_data.py
+++ tasks/load_data.py
@@ -1,6 +1,7 @@
-import csv
+import sys
 import gzip
 import glob
+import pandas as pd
 from datetime import datetime
 from hypergol import Source
 from data_models.raw_data import RawData
@@ -8,49 +9,46 @@ from data_models.raw_data import RawData
 
 class LoadData(Source):
 
-    def __init__(self, filePattern, *args, **kwargs):
+    def __init__(self, filePattern, logAtEachN, *args, **kwargs):
         super(LoadData, self).__init__(*args, **kwargs)
         # for example: '/data/hn-full-20201129/hn-full-20201129-*'
         self.filePattern = filePattern
-        self.intColumnsWithEmptyString = set()
-        self.intColumnsWithMinusOne = set()
+        self.logAtEachN = logAtEachN
 
     def source_iterator(self):
         hnfiles = glob.glob(self.filePattern)
         for hnfile in hnfiles:
-            with gzip.open(hnfile, 'rt') as csvfile:
-                for row in csv.DictReader(csvfile):
-                    yield row
-        self.logger.log(f'Found special columns:\n  intColumnsWithEmptyString:{self.intColumnsWithEmptyString}\n  intColumnsWithMinusOne:{self.intColumnsWithMinusOne}')
+            hnDataframe=pd.read_csv(gzip.open(hnfile,'rt'), dtype=object, na_filter=False)
+            for k, row in enumerate(hnDataframe.itertuples(index=False)):
+                if k % self.logAtEachN == 0:
+                    self.logger.log(f'Processed: {hnfile}: {k}/{len(hnDataframe)}')
+                yield row
 
     def run(self, data):
-        def safe_int(columnName, value):
+        def safe_int(value):
             if value == '':
-                self.intColumnsWithEmptyString.add(columnName)
-                return -1
-            if value =='-1':
-                self.intColumnsWithMinusOne.add(columnName)
+                return -sys.maxsize
             return int(value)
 
-        if data['timestamp'] == '':
+        if data.timestamp == '':
             timestamp = datetime.min
         else:
-            timestamp = datetime.fromisoformat(data['timestamp'][:19].replace(' ','T'))
+            timestamp = datetime.fromisoformat(data.timestamp[:19].replace(' ','T'))
 
         rawData = RawData(
-            title=data['title'], 
-            url=data['url'], 
-            text=data['text'], 
-            dead=1 if data['dead']=='true' else 0,
-            author=data['by'],
-            score=safe_int('score', data['score']),
-            time=safe_int('time', data['time']),
+            title=data.title, 
+            url=data.url, 
+            text=data.text, 
+            dead=1 if data.dead=='true' else 0,
+            author=data.by,
+            score=safe_int(data.score),
+            time=safe_int(data.time),
             timestamp=timestamp,
-            htype=data['type'],
-            hid=int(data['id']),
-            parent=safe_int('parent', data['parent']),
-            descendants=safe_int('descendants', data['descendants']),
-            ranking=safe_int('ranking', data['ranking']),
-            deleted=1 if data['deleted']=='true' else 0
+            htype=data.type,
+            hid=int(data.id),
+            parent=safe_int(data.parent),
+            descendants=safe_int(data.descendants),
+            ranking=safe_int(data.ranking),
+            deleted=1 if data.deleted=='true' else 0
         )
         return rawData
