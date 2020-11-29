1259365304539097f696181edbfdf28e6b9ac7c4
Laszlo Sragner <sragner@gmail.com>
2020-11-29 17:49:59 +0000

    attempt for faster load

--- tasks/load_data.py
+++ tasks/load_data.py
@@ -18,37 +18,37 @@ class LoadData(Source):
     def source_iterator(self):
         hnfiles = glob.glob(self.filePattern)
         for hnfile in hnfiles:
-            hnDataframe=pd.read_csv(gzip.open(hnfile,'rt'))
+            hnDataframe=pd.read_csv(gzip.open(hnfile,'rt'), dtype=object)
             for k, row in enumerate(hnDataframe.itertuples(index=False)):
                 if k % self.logAtEachN == 0:
                     self.logger.log(f'Processed: {hnfile}: {k}/{len(hnDataframe)}')
                 yield row
 
     def run(self, data):
-        def safe_int(columnName, value):
+        def safe_int(value):
             if value == '':
                 return -sys.maxsize
             return int(value)
 
-        if data['timestamp'] == '':
+        if data.timestamp == '':
             timestamp = datetime.min
         else:
-            timestamp = datetime.fromisoformat(data['timestamp'][:19].replace(' ','T'))
+            timestamp = datetime.fromisoformat(data.timestamp[:19].replace(' ','T'))
 
         rawData = RawData(
-            title=data['title'], 
-            url=data['url'], 
-            text=data['text'], 
-            dead=1 if data['dead']=='true' else 0,
-            author=data['by'],
-            score=safe_int('score', data['score']),
-            time=safe_int('time', data['time']),
+            title=data.title, 
+            url=data.url, 
+            text=data.text., 
+            dead=1 if data.dead=='true' else 0,
+            author=data.by,
+            score=safe_int(data.score),
+            time=safe_int(data.time),
             timestamp=timestamp,
-            htype=data['type'],
-            hid=int(data['id']),
-            parent=safe_int('parent', data['parent']),
-            descendants=safe_int('descendants', data['descendants']),
-            ranking=safe_int('ranking', data['ranking']),
-            deleted=1 if data['deleted']=='true' else 0
+            htype=data.type,
+            hid=int(data.id),
+            parent=safe_int(data.parent),
+            descendants=safe_int(data.descendants),
+            ranking=safe_int(data.ranking.),
+            deleted=1 if data.deleted=='true' else 0
         )
         return rawData
