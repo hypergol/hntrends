7ac4ff3e9b5a92d035454b7b27c6c0bfdcd2958e
Laszlo Sragner <sragner@gmail.com>
2020-11-29 14:16:32 +0000

    CleanText and ProcessWithSpacy added - redone to make TaskType=Simple

--- tasks/clean_text.py
+++ tasks/clean_text.py
@@ -1,18 +1,18 @@
-from hypergol import Source
+from hypergol import SimpleTask
 
 
-class CleanText(Source):
+class CleanText(SimpleTask):
 
     def __init__(self, exampleParameter, *args, **kwargs):
         super(CleanText, self).__init__(*args, **kwargs)
-        # TODO: Source tasks are single threaded, no need for members to be pickle-able
+        # TODO: all member variables must be pickle-able, otherwise use the "Delayed" methodology
+        # TODO: (e.g. for a DB connection), see the documentation <add link here>
         self.exampleParameter = exampleParameter
 
-    def source_iterator(self):
-        raise NotImplementedError(f'{self.__class__.__name__} must implement source_iterator()')
-        # TODO: use yield in this function instead of return while your are consuming your source data
-        yield exampleData
+    def init(self):
+        # TODO: initialise members that are NOT "Delayed" here (e.g. load spacy model)
+        pass
 
-    def run(self, data):
+    def run(self, exampleInputObject1, exampleInputObject2):
         raise NotImplementedError(f'{self.__class__.__name__} must implement run()')
         return exampleOutputObject
