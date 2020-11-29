af019e955cccaa6bfb991a6b276af4e57ac98962
Laszlo Sragner <sragner@gmail.com>
2020-11-29 14:12:27 +0000

    LoadData finished

--- tasks/load_data.py
+++ tasks/load_data.py
@@ -1,18 +1,39 @@
+import gzip
+import glob
+from datetime import datetime
 from hypergol import Source
+from data_model.raw_data import RawData
 
 
 class LoadData(Source):
 
-    def __init__(self, exampleParameter, *args, **kwargs):
+    def __init__(self, filePattern, *args, **kwargs):
         super(LoadData, self).__init__(*args, **kwargs)
-        # TODO: Source tasks are single threaded, no need for members to be pickle-able
-        self.exampleParameter = exampleParameter
+        # for example: '/data/hn-full-20201129/hn-full-20201129-*'
+        self.filePattern = filePattern
 
     def source_iterator(self):
-        raise NotImplementedError(f'{self.__class__.__name__} must implement source_iterator()')
-        # TODO: use yield in this function instead of return while your are consuming your source data
-        yield exampleData
+        hnfiles = glob.glob(filePattern)
+        for hnfile in hnfiles:
+            with gzip.open(hnfile, 'rt') as csvfile:
+                for row in csv.DictReader(csvfile):
+                    yield row
 
     def run(self, data):
-        raise NotImplementedError(f'{self.__class__.__name__} must implement run()')
+        rawData = RawData(
+            title=data['title'], 
+            url=data['url'], 
+            text=data['text'], 
+            dead=1 if data['dead']=='true' else 0,
+            author=data['by'],
+            score=int(data['score']),
+            time=int(data['time']),
+            timestamp=datetime.fromisoformat(data['timestamp'][:19].replace(' ','T')),
+            htype=data['type'],
+            hid=int(data['id']),
+            parent=int(data['parent']),
+            descendants=int(data['descendants']),
+            ranking=int(data['ranking'])
+            deleted=1 if data['deleted']=='true' else 0
+        )
         return exampleOutputObject
