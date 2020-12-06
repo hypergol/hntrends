496c85452626f42305ed5dcfada4adcd3b9fad48
Laszlo Sragner <sragner@gmail.com>
2020-12-06 16:02:36 +0000

    comment added

---
+++ data_models/comment.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from hypergol import BaseData
+
+
+class Comment(BaseData):
+
+    def __init__(self, text: str, author: str, time: int, timestamp: datetime, hid: int, parent: int):
+        self.text = text
+        self.author = author
+        self.time = time
+        self.timestamp = timestamp
+        self.hid = hid
+        self.parent = parent
+
+    def get_id(self):
+        return (self.hid, )
+
+    def to_data(self):
+        data = self.__dict__.copy()
+        data['timestamp'] = data['timestamp'].isoformat()
+        return data
+
+    @classmethod
+    def from_data(cls, data):
+        data['timestamp'] = datetime.fromisoformat(data['timestamp'])
+        return cls(**data)
