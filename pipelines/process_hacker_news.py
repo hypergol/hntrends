e320def06309e03a959b4d2861f32217c39f5a43
Laszlo Sragner <sragner@gmail.com>
2020-12-06 17:00:50 +0000

    doing comments

--- pipelines/process_hacker_news.py
+++ pipelines/process_hacker_news.py
@@ -38,8 +38,8 @@ def process_hacker_news(filePattern, dataDirectory, threads=1, raiseIfDirty=True
     pipeline = Pipeline(
         tasks=[
             # loadData,
-            selectStories,
-            # selectComments,
+            # selectStories,
+            selectComments,
         ]
     )
     pipeline.run(threads=threads)
