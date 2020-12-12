490a75b05dc28a15d7b3e90152aca931d5f1948f
Laszlo Sragner <sragner@gmail.com>
2020-12-12 12:40:06 +0000

    vocab done

--- models/train_embedding_model.py
+++ models/train_embedding_model.py
@@ -5,6 +5,7 @@ import logging
 import array
 import numpy as np
 import scipy.sparse as sps
+from itertools import islice
 from tqdm import tqdm
 from hypergol import HypergolProject
 from hypergol.logger import Logger
@@ -30,24 +31,15 @@ class EpochSaver(CallbackAny2Vec):
         model.save(fileName)
         self.epoch += 1
 
-
-def train_embedding_model(dataDirectory, threads=1, force=False): 
-    logger = Logger()
-    project = HypergolProject(dataDirectory=dataDirectory, force=force)
-    documentsDataset = project.datasetFactory.get(
-        dataType=Document, 
-        branch='document_creation', 
-        name='documents', 
-        chunkCount=256
-    )
-
-    logger.info('Create vocabulary and matrix - START')
+def create_vocabulary(documentsDataset, logger):
+    logger.info('Process documents - START')
     vocabulary = {}
     rows=array.array('I')
     cols=array.array('I')
     mentionId = 1_000_000_0000
     with documentsDataset.open('r') as dsr:
-        for document in tqdm(dsr, total=21_000_000):
+        # for document in tqdm(dsr, total=21_000_000):
+        for document in tqdm(islice(dsr, 1_000_000), total=1_000_000):
             hid = f'hn{document.hid}'
             for label in document.labels:
                 if label not in vocabulary:
@@ -68,35 +60,47 @@ def train_embedding_model(dataDirectory, threads=1, force=False):
                 cols.append(vocabulary[hid])
                 lastMentionId = mentionId
                 mentionId += 1
-    logger.info('Create vocabulary and matrix - END')
-    pickle.dump(vocabulary, '')
-    rows = np.frombuffer(rows, dtype=np.int32)
-    np.save(rows, '')
-    logger.info('Create vocabulary and matrix - END')
+    logger.info('Process documents - save vocabulary')
+    pickle.dump(vocabulary, open(f'{dataDirectory}/vocabulary.pkl', 'rb'))
+    logger.info('Process documents - save rows - END')
+    np.save(f'{dataDirectory}/rows.npy', np.frombuffer(rows, dtype=np.int32))
+    logger.info('Process documents - save cols - END')
+    np.save(f'{dataDirectory}/cols.npy', np.frombuffer(cols, dtype=np.int32))
+    logger.info('Process documents - END')
 
+def train_embedding_model(dataDirectory, threads=1, force=False): 
+    logger = Logger()
+    project = HypergolProject(dataDirectory=dataDirectory, force=force)
+    documentsDataset = project.datasetFactory.get(
+        dataType=Document, 
+        branch='document_creation', 
+        name='documents', 
+        chunkCount=256
+    )
 
+    create_vocabulary(documentsDataset=documentsDataset, logger=logger)
 
 
     
-    epochSaver = EpochSaver(
-        modelDirectory=project.datasetFactory.branchDirectory,
-        modelName=f'doc2vec_{date.today().strftime("%Y%m%d")}_{project.repoManager.commitHash}'
-    )
+    # epochSaver = EpochSaver(
+    #     modelDirectory=project.datasetFactory.branchDirectory,
+    #     modelName=f'doc2vec_{date.today().strftime("%Y%m%d")}_{project.repoManager.commitHash}'
+    # )
 
 
 
-    logger.info('Model construction - START')
-    model = Doc2Vec(
-        dm=0, dbow_words=1, dm_concat=0, vector_size=VECTOR_SIZE, window=5, 
-        negative=20, hs=0, min_count=3, workers=31, 
-        epochs=50, alpha=0.025, min_alpha=0.001, callbacks=[epochSaver]
-    )
-    model.build_vocab(taggedData)
-    logger.info('Model construction - END')
+    # logger.info('Model construction - START')
+    # model = Doc2Vec(
+    #     dm=0, dbow_words=1, dm_concat=0, vector_size=VECTOR_SIZE, window=5, 
+    #     negative=20, hs=0, min_count=3, workers=31, 
+    #     epochs=50, alpha=0.025, min_alpha=0.001, callbacks=[epochSaver]
+    # )
+    # model.build_vocab(taggedData)
+    # logger.info('Model construction - END')
 
-    logger.info('Model training - START')
-    model.train(documents=taggedData, total_examples=model.corpus_count, epochs=model.epochs)
-    logger.info('Model training - END')
+    # logger.info('Model training - START')
+    # model.train(documents=taggedData, total_examples=model.corpus_count, epochs=model.epochs)
+    # logger.info('Model training - END')
 
 if __name__ == '__main__':
     fire.Fire(train_embedding_model)
